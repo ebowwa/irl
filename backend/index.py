d08b20885797cf4cdc3c2ad05f1a460ef5183c41
ebowwa <ec.arbee1@gmail.com>
2024-11-16 14:38:54 -0800

    index.py cleaned

--- backend/index.py
+++ backend/index.py
@@ -2,7 +2,7 @@
 # You can make an audio file available to Gemini in either of the following ways:\n\nUpload the audio file prior to making the prompt request.\nProvide the audio file as inline data to the prompt request.
 import logging
 import os
-# TODO: need some way to have server update clients(nextjs website, swift apps) on the servers url and other constants
+# TODO: need endpoint to have server update clients(nextjs website, swift apps) on the servers url and other constants
 import ngrok
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException, Request
@@ -10,20 +10,10 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.responses import JSONResponse
 from route.dev import socket_ping
-# from route.post.audio.transcription.falIndex import router as transcription_router removed to use gemini
-# from route.features.gemini.gemini_post_v1 import router as gemini_router
-# from route.features.gemini.gemini_post_v1B import router as gemini_v1B_router
-# from route.features.gemini.gemini_post_v2 import router as gemini_post_v2_router
-# from route.features.gemini.gemini_postv3 import router as gemini_post_v3_router
-# from route.features.gemini.@depreciated.gemini_socket import router as gemini_socket_router
 from route.features.gemini.gemini_transcription_v1 import router as gemini_transcription_v1_router
 from route.features.gemini.truth_n_lie_v1 import router as analyze_truth_lie_v1_router
-# from route.features.gemini.user_name_upload_v1 import router as user_name_upload_v1_router
-# from route.features.gemini.user_name_upload_v2 import router as user_name_upload_v2_router
-# from route.features.gemini.user_name_upload_v3 import router as user_name_upload_v3_router
-# from route.features.gemini.user_name_upload_v4 import router as user_name_upload_v4_router
 from route.features.gemini.user_name_upload_v5 import router as user_name_upload_v5_router
-from route.features.gemini.gemini_audio_handling import router as user_name_upload_v6_router
+from route.features.gemini.gemini_audio_handling import router as gemini_audio_handling_router
 from route.features.gemini.google_media_upload import router as google_media_upload_router
 from route.features.humeclient import router as hume_router
 from route.features.gemini.google_media_upload_v2 import router as google_media_upload_v2_router
@@ -79,15 +69,13 @@ app.include_router(share_oai_chats_router, prefix="/retrieve/externalchats")  # 
 
 
 ### ----------------------------------------------------------------------
-##      NEXT.JS SITEß
+##      NEXT.JS SITE
 app.include_router(web_waitlist_crud_router) # CRUD backend/data/waitlist_data.db
 
 ### -----------------------------------------------------------------------
 ##      ONBOARDING (NO-AUTH)
-# app.include_router(user_name_upload_v3_router, prefix="/onboarding/v3")  # + /process-audio; TODO: on client side implement double-try correct user name
-# app.include_router(user_name_upload_v4_router, prefix="/onboarding/v4")  # + /process-audio; TODO: on client side implement double-try correct user name
-app.include_router(user_name_upload_v5_router, prefix="/onboarding/v5") 
-app.include_router(user_name_upload_v6_router, prefix="/onboarding/v6")
+app.include_router(user_name_upload_v5_router, prefix="/onboarding/v5")   # + /process-audio; TODO: on client side implement double-try correct user name
+app.include_router(gemini_audio_handling_router, prefix="/onboarding/v6")
 app.include_router(analyze_truth_lie_v1_router)
 # TODO: one-liner & Day in the life Q's
 # -------------------------------------------------------------------------
@@ -96,17 +84,10 @@ app.include_router(device_registration_router, prefix="/device") # CRUD backend/
 
 ### ------------------------------------------------------------------------
 ##      GEMINI
-# app.include_router(gemini_router, prefix="/v1/post/gemini")
-# app.include_router(gemini_v1B_router, prefix="/v1B/post/gemini")
-
-# app.include_router(gemini_post_v2_router, prefix="/v2/post/gemini")
-# app.include_router(gemini_socket_router, prefix="/socket/gemini")
 app.include_router(gemini_transcription_v1_router, prefix="/gemini")  # + add `/ws/transcribe`
 app.include_router(google_media_upload_router) # `https://server/upload-to-gemini`
 app.include_router(google_media_upload_v2_router, prefix="/v2") # `https://server/v2/upload-to-gemini`
 app.include_router(google_media_upload_v3_router, prefix="/v3")
-# app.include_router(gemini_post_v3_router, prefix="/v3") 
-
 
 # ------------------ OpenAPI & Swagger UI ---------------------------
 
