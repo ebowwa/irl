23bcab6fd758efbc5527ded2dd447ab6035ddd67
ebowwa <basedebowwa@gmail.com>
2024-11-29 17:16:06 -0800

    web

--- clients/caringmindWeb/README.md
+++
@@ -1,3 +0,0 @@
-# sb1-bhwhxv
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ebowwa/sb1-bhwhxv)
--- clients/caringmindWeb/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { LandingPage } from "@/components/pages/LandingPage";
-
-export default function Home() {
-  return <LandingPage />;
-}
